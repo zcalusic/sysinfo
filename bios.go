0533dcd0c31f415250514e9902a9406188250b84
chatenilesh <chatenilesh@gmail.com>
2019-02-11 14:52:54 +0530

    Added support for Redhat release in OS info Added support for SMBIOS version in BIOS info

--- bios.go
+++ bios.go
@@ -4,15 +4,34 @@
 
 package sysinfo
 
+import (
+	"fmt"
+	"github.com/digitalocean/go-smbios/smbios"
+)
+
 // BIOS information.
 type BIOS struct {
-	Vendor  string `json:"vendor,omitempty"`
-	Version string `json:"version,omitempty"`
-	Date    string `json:"date,omitempty"`
+	Vendor        string `json:"vendor,omitempty"`
+	Version       string `json:"version,omitempty"`
+	Date          string `json:"date,omitempty"`
+	SmbiosVersion string `json:"SmbiosVersion,omitempty"`
 }
 
 func (si *SysInfo) getBIOSInfo() {
 	si.BIOS.Vendor = slurpFile("/sys/class/dmi/id/bios_vendor")
 	si.BIOS.Version = slurpFile("/sys/class/dmi/id/bios_version")
 	si.BIOS.Date = slurpFile("/sys/class/dmi/id/bios_date")
+	si.BIOS.SmbiosVersion = getSmbiosVersion()
+}
+
+func getSmbiosVersion() string {
+	var smbiosVersion string
+	if rc, ep, err := smbios.Stream(); err == nil {
+		defer rc.Close()
+
+		major, minor, rev := ep.Version()
+		smbiosVersion = fmt.Sprintf("%d.%d.%d", major, minor, rev)
+	}
+
+	return smbiosVersion
 }
--- go.mod
+++ go.mod
@@ -1,1 +1,3 @@
 module github.com/zcalusic/sysinfo
+
+require github.com/digitalocean/go-smbios v0.0.0-20180907143718-390a4f403a8e
