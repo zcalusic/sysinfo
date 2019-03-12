9c0585c24968bd8dabcfc46eb575189a71e827ae
腹黒い茶 <haraguroicha@tipsy.coffee>
2019-03-12 15:37:48 +0800

    try fallback to use sysfs of DMI block to decode memory info

--- memory.go
+++ memory.go
@@ -9,6 +9,7 @@ import (
 	"bytes"
 	"encoding/binary"
 	"errors"
+	"io/ioutil"
 	"os"
 	"strconv"
 	"strings"
@@ -125,7 +126,11 @@ func getStructureTableAddress(f *os.File) (address int64, length int, err error)
 func getStructureTable() ([]byte, error) {
 	f, err := os.Open("/dev/mem")
 	if err != nil {
-		return nil, err
+		dmi, err := ioutil.ReadFile("/sys/firmware/dmi/tables/DMI")
+		if err != nil {
+			return nil, err
+		}
+		return dmi, nil
 	}
 	defer f.Close()
 
