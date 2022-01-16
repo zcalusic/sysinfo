596ebe2a409b3632ef3bf7a46ea765e4794cb16a
zhengge2012 <zhengge2012@gmail.com>
2022-01-16 19:55:32 +0800

    add centos 5/6 and red hate 5/6 system support

--- os.go
+++ os.go
@@ -6,6 +6,7 @@ package sysinfo
 
 import (
 	"bufio"
+	"io/ioutil"
 	"os"
 	"regexp"
 	"strings"
@@ -29,6 +30,69 @@ var (
 	reRedHat     = regexp.MustCompile(`[\( ]([\d\.]+)`)
 )
 
+func file_is_exists(f string) bool {
+	_, err := os.Stat(f)
+	if os.IsNotExist(err) {
+		return false
+	}
+	return err == nil
+}
+
+func getCentosOrRedHatOldSystem(si *SysInfo) {
+	// centos 5/6 or redhat enterprise 5/6 need to use /etc/redhat-release to detect system info
+	fileName := "/etc/redhat-release"
+	// detect if the system is centos or red hat
+	if si.OS.Vendor == "" {
+		data, err := ioutil.ReadFile(fileName)
+		if err != nil {
+			return
+		}
+		if strings.Contains(string(data), "CentOS") {
+			si.OS.Vendor = "centos"
+		}
+
+		if strings.Contains(string(data), "Red Hat") {
+			si.OS.Vendor = "rhel"
+		}
+	}
+	switch si.OS.Vendor {
+	case "centos":
+		{
+			// if centos release not exist then use red hat file
+			if file_is_exists("/etc/centos-release") {
+				fileName = "/etc/centos-release"
+			}
+			if release := slurpFile(fileName); release != "" {
+				if m := reCentOS.FindStringSubmatch(release); m != nil {
+					si.OS.Release = m[2]
+				}
+			}
+		}
+	case "rhel":
+		{
+			if release := slurpFile("/etc/redhat-release"); release != "" {
+				if m := reRedHat.FindStringSubmatch(release); m != nil {
+					si.OS.Release = m[1]
+				}
+			}
+			if si.OS.Release == "" {
+				if m := reRedHat.FindStringSubmatch(si.OS.Name); m != nil {
+					si.OS.Release = m[1]
+				}
+			}
+		}
+	}
+	// fix centos 5/6 and red hat 5/6 release bug
+	if si.OS.Release != "" {
+		if strings.HasPrefix(si.OS.Release, "5.") {
+			si.OS.Version = "5"
+		}
+		if strings.HasPrefix(si.OS.Release, "6.") {
+			si.OS.Version = "6"
+		}
+	}
+}
+
 func (si *SysInfo) getOSInfo() {
 	// This seems to be the best and most portable way to detect OS architecture (NOT kernel!)
 	if _, err := os.Stat("/lib64/ld-linux-x86-64.so.2"); err == nil {
@@ -39,6 +103,7 @@ func (si *SysInfo) getOSInfo() {
 
 	f, err := os.Open("/etc/os-release")
 	if err != nil {
+		getCentosOrRedHatOldSystem(si)
 		return
 	}
 	defer f.Close()
