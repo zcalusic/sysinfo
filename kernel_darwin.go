6782e1f5e11ab06b822a8de521e6bf7081b22e87
Itxaka <igarcia@suse.com>
2022-02-17 14:34:28 +0100

    support building on darwin
    Signed-off-by: Itxaka <igarcia@suse.com>

---
+++ kernel_darwin.go
@@ -0,0 +1,13 @@
+//+build darwin
+
+package sysinfo
+
+// Kernel information.
+type Kernel struct {
+	Release      string `json:"release,omitempty"`
+	Version      string `json:"version,omitempty"`
+	Architecture string `json:"architecture,omitempty"`
+}
+
+func (si *SysInfo) getKernelInfo() {
+}
