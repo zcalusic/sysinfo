4eac1cff6379dbdc307815a25f48562ab269b723
Bipin B Narayan <bipin.narayan@qubecinema.com>
2023-10-11 19:24:49 +0530

    Include product SKU and UUID in Product information

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/zcalusic/sysinfo
 
 go 1.20
+
+require github.com/google/uuid v1.3.1
--- product.go
+++ product.go
@@ -4,12 +4,16 @@
 
 package sysinfo
 
+import "github.com/google/uuid"
+
 // Product information.
 type Product struct {
-	Name    string `json:"name,omitempty"`
-	Vendor  string `json:"vendor,omitempty"`
-	Version string `json:"version,omitempty"`
-	Serial  string `json:"serial,omitempty"`
+	Name    string    `json:"name,omitempty"`
+	Vendor  string    `json:"vendor,omitempty"`
+	Version string    `json:"version,omitempty"`
+	Serial  string    `json:"serial,omitempty"`
+	UUID    uuid.UUID `json:"uuid,omitempty"`
+	SKU     string    `json:"sku,omitempty"`
 }
 
 func (si *SysInfo) getProductInfo() {
@@ -17,4 +21,10 @@ func (si *SysInfo) getProductInfo() {
 	si.Product.Vendor = slurpFile("/sys/class/dmi/id/sys_vendor")
 	si.Product.Version = slurpFile("/sys/class/dmi/id/product_version")
 	si.Product.Serial = slurpFile("/sys/class/dmi/id/product_serial")
+	si.Product.SKU = slurpFile("/sys/class/dmi/id/product_sku")
+
+	uid, err := uuid.Parse(slurpFile("/sys/class/dmi/id/product_uuid"))
+	if err == nil {
+		si.Product.UUID = uid
+	}
 }
